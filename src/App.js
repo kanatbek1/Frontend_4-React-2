62ddd97db965e1b3c3bb59b859f943e5a0607b9d
kanatbek1 <130185554+kanatbek1@users.noreply.github.com>
2023-06-24 16:39:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
+import TodoApp from "./pages/TodoApp";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      stateRed: true,
-      stateBlue: false,
-    };
-  }
-
-  handleClick = () => {
-    this.setState((prevState) => ({
-      stateRed: !prevState.stateRed,
-      stateBlue: !prevState.stateBlue,
-    }));
-  };
-
-  render() {
-    const { stateRed, stateBlue } = this.state;
-
-    const square1Style = {
-      width: "200px",
-      height: "200px",
-      backgroundColor: stateRed ? "red" : "blue",
-      cursor: "pointer",
-    };
-
-    const square2Style = {
-      width: "200px",
-      height: "200px",
-      backgroundColor: stateBlue ? "red" : "blue",
-      cursor: "pointer",
-    };
-
-    return (
-      <>
-        <div
-          className="color-square"
-          style={square1Style}
-          onClick={this.handleClick}></div>
-
-        <div
-          className="color-square"
-          style={square2Style}
-          onClick={this.handleClick}></div>
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <TodoApp />
+    </>
+  );
 }
 
 export default App;
